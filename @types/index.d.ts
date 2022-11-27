9955a0551f611a592682cae6e8a46a05826bfc6c
g-ddy <giddygeedy12@gmail.com>
2022-11-28 02:08:28 +0700

    Add DOMPurify and marked type dependencies.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,6 +1,10 @@
 import type dom from 'cash-dom';
+import type { marked as __marked } from 'marked';
+import DOMPurify from 'dompurify';
 
 declare global {
 	export var $: typeof dom;
+	export var marked: typeof __marked;
+	export var DOMPurify: typeof DOMPurify;
 }
 
