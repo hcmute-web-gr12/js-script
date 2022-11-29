c741cd0ff416674b385e1a7307c84724e459bb5d
g-ddy <giddygeedy12@gmail.com>
2022-11-30 04:21:53 +0700

    Handle pet delete.

--- src/admin/pet.ts
+++ src/admin/pet.ts
@@ -1,6 +1,11 @@
-import { prependSpinner } from 'lib/spinner';
+import { disableClick } from 'lib/disableClick';
+import { makeSpinner, prependSpinner } from 'lib/spinner';
 
 let submitting = false;
+const deleteScheduler: { counter: number, shouldUpdate: boolean } = {
+	counter: 0,
+	shouldUpdate: false
+};
 
 async function submit(this: HTMLFormElement, ev: Event) {
 	ev.preventDefault();
@@ -11,7 +16,7 @@ async function submit(this: HTMLFormElement, ev: Event) {
 	const dialog = document.getElementById('pet-dialog');
 	if (dialog instanceof HTMLDialogElement) {
 		const button = document.getElementById('pet-add-submit') as HTMLButtonElement;
-		const remove = prependSpinner(button);
+		const remove = prependSpinner(button, 'w-4', 'h-4', 'mr-2');
 		const res = await fetch(this.action, { method: 'post', body: new FormData(this) });
 		remove();
 		submitting = false;
@@ -46,17 +51,61 @@ async function updatePets() {
 	if (!(table instanceof HTMLTableElement)) {
 		return;
 	}
+	const enableClick = disableClick(table);
 	table.classList.add('animate-pulse');
 	const html = await fetch('/api/admin/pet').then(v => v.text());
+	enableClick();
 
 	table.querySelector('tbody')?.remove();
 	table.querySelector('tfoot')?.remove();
-	table.innerHTML += html;
+	table.insertAdjacentHTML('beforeend', html);
+	addEditAndDeleteEvents();
 	setTimeout(() => {
 		table.classList.remove('animate-pulse');
-	}, 300);
+	}, 100);
+}
+
+function editPet(this: HTMLButtonElement, ev: Event) {
+
+}
+
+async function deletePet(this: HTMLButtonElement) {
+	console.log(this);
+	const index = this.id.replace('delete-pet-', '');
+	const html = this.innerHTML;
+	const spinner = makeSpinner('w-5', 'h-5');
+	this.replaceChildren(spinner);
+
+	++deleteScheduler.counter;
+	const status = (await fetch(`/api/admin/pet?index=${index}`, { method: 'delete' })).status;
+	--deleteScheduler.counter;
+
+	spinner.remove();
+	this.innerHTML = html;
+	if (status === 200) {
+		this.disabled = true;
+		deleteScheduler.shouldUpdate = true;
+		if (deleteScheduler.counter) {
+			return;
+		}
+		if (deleteScheduler.shouldUpdate) {
+			deleteScheduler.shouldUpdate = false;
+			updatePets();
+		}
+	}
+}
+
+function addEditAndDeleteEvents() {
+	for(const btn of document.getElementsByClassName('edit-pet') as any as HTMLButtonElement[] || []) {
+		btn.addEventListener('click', editPet);
+	}
+
+	for(const btn of document.getElementsByClassName('delete-pet') as any as HTMLButtonElement[] || []) {
+		btn.addEventListener('click', deletePet);
+	}
 }
 
 document.getElementById('pet-form')?.addEventListener('submit', submit);
 document.getElementById('pet-add')?.addEventListener('pointerdown', showDialog);
 document.getElementById('pet-form-back')?.addEventListener('pointerdown', closeDialog);
+addEditAndDeleteEvents();
