d9a60d01a1ee441654c68d262f1eeead759f35f4
g-ddy <giddygeedy12@gmail.com>
2022-11-29 20:32:29 +0700

    Display spinner on fetching; Update pets table.

--- src/admin/pet.ts
+++ src/admin/pet.ts
@@ -1,3 +1,5 @@
+import { prependSpinner } from 'lib/spinner';
+
 let submitting = false;
 
 async function submit(this: HTMLFormElement, ev: Event) {
@@ -8,9 +10,13 @@ async function submit(this: HTMLFormElement, ev: Event) {
 	submitting = true;
 	const dialog = document.getElementById('pet-dialog');
 	if (dialog instanceof HTMLDialogElement) {
+		const button = document.getElementById('pet-add-submit') as HTMLButtonElement;
+		const remove = prependSpinner(button);
 		const res = await fetch(this.action, { method: 'post', body: new FormData(this) });
+		remove();
 		submitting = false;
 		if (res.status === 200) {
+			updatePets();
 			dialog.close();
 			return;
 		}
@@ -33,6 +39,22 @@ function closeDialog(this: HTMLButtonElement) {
 	if (dialog instanceof HTMLDialogElement) {
 		dialog.close();
 	}
+}
+
+async function updatePets() {
+	const table = document.getElementById('pet-table');
+	if (!(table instanceof HTMLTableElement)) {
+		return;
+	}
+	table.classList.add('animate-pulse');
+	const html = await fetch('/api/admin/pet').then(v => v.text());
+
+	table.querySelector('tbody')?.remove();
+	table.querySelector('tfoot')?.remove();
+	table.innerHTML += html;
+	setTimeout(() => {
+		table.classList.remove('animate-pulse');
+	}, 300);
 }
 
 document.getElementById('pet-form')?.addEventListener('submit', submit);
