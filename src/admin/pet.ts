55b48ae66df4d1d3b728143a19117cbae5ef6d41
g-ddy <giddygeedy12@gmail.com>
2022-12-01 20:26:17 +0700

    Implement select all and delete some.

--- src/admin/pet.ts
+++ src/admin/pet.ts
@@ -59,7 +59,7 @@ async function updatePets() {
 	table.querySelector('tbody')?.remove();
 	table.querySelector('tfoot')?.remove();
 	table.insertAdjacentHTML('beforeend', html);
-	addEditAndDeleteEvents();
+	addTableEvents();
 	setTimeout(() => {
 		table.classList.remove('animate-pulse');
 	}, 100);
@@ -69,15 +69,18 @@ function editPet(this: HTMLButtonElement, ev: Event) {
 
 }
 
+function buildDeleteRequest(...indexes: number[]) {
+	return `/api/admin/pet?${indexes.map(index => `index=${index}`).join('&')}`;
+}
+
 async function deletePet(this: HTMLButtonElement) {
-	console.log(this);
 	const index = this.id.replace('delete-pet-', '');
 	const html = this.innerHTML;
 	const spinner = makeSpinner('w-5', 'h-5');
 	this.replaceChildren(spinner);
 
 	++deleteScheduler.counter;
-	const status = (await fetch(`/api/admin/pet?index=${index}`, { method: 'delete' })).status;
+	const status = (await fetch(buildDeleteRequest(+index), { method: 'delete' })).status;
 	--deleteScheduler.counter;
 
 	spinner.remove();
@@ -95,17 +98,84 @@ async function deletePet(this: HTMLButtonElement) {
 	}
 }
 
-function addEditAndDeleteEvents() {
-	for(const btn of document.getElementsByClassName('edit-pet') as any as HTMLButtonElement[] || []) {
+function addTableEvents() {
+	for (const btn of document.getElementsByClassName('edit-pet') as any as HTMLButtonElement[] || []) {
 		btn.addEventListener('click', editPet);
 	}
 
-	for(const btn of document.getElementsByClassName('delete-pet') as any as HTMLButtonElement[] || []) {
+	for (const btn of document.getElementsByClassName('delete-pet') as any as HTMLButtonElement[] || []) {
 		btn.addEventListener('click', deletePet);
+	}
+
+	for (const checkbox of document.getElementsByClassName('pet-select') as any as HTMLInputElement[] || []) {
+		checkbox.addEventListener('input', toggleSelect);
+	}
+}
+
+function toggleSelectAll(this: HTMLInputElement) {
+	for (const checkbox of document.getElementsByClassName('pet-select') as any as HTMLInputElement[]) {
+		checkbox.checked = this.checked;
+	}
+}
+
+function toggleSelect(this: HTMLInputElement) {
+	const selectAllCheckbox = document.getElementById('pet-select-all') as HTMLInputElement | undefined;
+	if (selectAllCheckbox && selectAllCheckbox.checked) {
+		if (!this.checked) {
+			selectAllCheckbox.checked = false;
+		}
+	}
+}
+
+async function deleteSomePets(this: HTMLButtonElement) {
+	const indexes: number[] = [];
+	const buttons: HTMLButtonElement[] = [];
+	for (const [k, v] of (Array.from(document.getElementsByClassName('pet-select')) as HTMLInputElement[]).entries() ) {
+		if (v.checked) {
+			buttons.push(document.getElementById(`delete-pet-${k}`) as HTMLButtonElement);
+			indexes.push(k);
+			console.log(k, document.getElementById(`delete-pet-${k}`));
+		}
+	}
+	if (!indexes.length) {
+		return;
+	}
+	const requestString = buildDeleteRequest(...indexes);
+	const htmls: string[] = [];
+	const promise = fetch(requestString, { method: 'delete' });
+	buttons.push(this);
+	for(const button of buttons) {
+		htmls.push(button.innerHTML);
+		const spinner = makeSpinner('w-5', 'h-5');
+		button.replaceChildren(spinner);
+		button.disabled = true;
+	}
+
+	++deleteScheduler.counter;
+	const status = (await promise).status;
+	--deleteScheduler.counter;
+
+	for(const [k, button] of buttons.entries()) {
+		button.firstElementChild!.remove();
+		button.innerHTML = htmls[k];
+		button.disabled = true;
+	}
+	if (status === 200) {
+		deleteScheduler.shouldUpdate = true;
+		if (deleteScheduler.counter) {
+			return;
+		}
+		if (deleteScheduler.shouldUpdate) {
+			deleteScheduler.shouldUpdate = false;
+			updatePets();
+		}
 	}
 }
 
 document.getElementById('pet-form')?.addEventListener('submit', submit);
 document.getElementById('pet-add')?.addEventListener('pointerdown', showDialog);
 document.getElementById('pet-form-back')?.addEventListener('pointerdown', closeDialog);
-addEditAndDeleteEvents();
+document.getElementById('pet-select-all')?.addEventListener('input', toggleSelectAll);
+document.getElementById('pet-select-all')?.addEventListener('input', toggleSelectAll);
+document.getElementById('pet-delete-some')?.addEventListener('click', deleteSomePets);
+addTableEvents();
