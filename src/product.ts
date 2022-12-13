30081ef7ad6c206b651436324162993f73344e28
g-ddy <giddygeedy12@gmail.com>
2022-12-14 00:02:21 +0700

    Add success message.

--- src/product.ts
+++ src/product.ts
@@ -19,20 +19,28 @@ document.
 
 function submit(this: HTMLFormElement, ev: Event) {
 	ev.preventDefault();
-	const error = document.getElementById('error') as HTMLParagraphElement;
+	const result = document.getElementById('form-result') as HTMLParagraphElement;
 	const btn = (document.getElementById('add-to-cart') as HTMLButtonElement);
 	const remove = appendSpinner(btn, 'w-4', 'h-4', 'ml-2');
 	const data = new FormData();
 	btn.disabled = true;
-	error.classList.add('opacity-0');
+	result.classList.add('opacity-0');
 	data.set('id', props.id);
 	fetch('/api/cart', {
 		method: 'post', body: new URLSearchParams(data as any)
-	}).then(v => {
-		if (v.status === 403) {
-			error.textContent = 'Xin vui lòng đăng nhập.';
-			error.classList.remove('opacity-0');
+	}).then(async v => {
+		result.classList.remove('opacity-0');
+		if (v.status === 200) {
+			result.classList.replace('text-red-600', 'text-green-600');
+			result.textContent = 'Thêm vào giỏ hàng thành công.';
 			return;
+		}
+
+		result.classList.replace('text-green-600', 'text-red-600');
+		if (v.status === 403) {
+			result.textContent = 'Xin vui lòng đăng nhập.';
+		} else {
+			result.textContent = await v.text();
 		}
 	}).finally(() => {
 		btn.disabled = false;
